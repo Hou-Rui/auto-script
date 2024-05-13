7f7d3e01f85aa1fe2e7e8d52153ebfac04eadfa4
Hou Rui <hourui@realtek-sg.com>
2024-05-13 18:12:19 +0800

    refactor structure

--- auto.py
+++ auto.py
@@ -3,7 +3,7 @@
 import click
 import os
 import json
-from typing import Any, Callable, Iterable
+from typing import Iterable
 
 
 with open('auto.json') as config_file:
@@ -22,60 +22,53 @@ def support_package(required: bool | None = True):
     return click.argument('package', nargs=-1, required=required)
 
 
-def get_source_configs(source_names: str | Iterable[str]):
-    if isinstance(source_names, str):
-        source_names = (source_names,)
+def get_source_configs(source_names: Iterable[str], action: str):
+    available_names = config['source'].keys()
     for name in source_names:
-        if name not in config['source']:
-            click.echo(f'Source not found: {name}')
+        if name not in available_names:
+            available_str = ' '.join(available_names)
+            click.secho(f'Error: Source "{name}" not available', err=True)
+            click.secho(f'Available sources: {available_str}', err=True)
             exit(1)
-        yield config['source'][name]
+        source = config['source'][name]
+        if action not in source:
+            continue
+        yield source[action]
 
 
-def subcommand(source_multiple: bool = True, package_required: bool | None = True):
-    def decorator(func: Callable[[], Any]):
-        name = func.__name__
-        @cli.command(name)
-        @support_source(multiple=source_multiple)
-        @support_package(required=package_required)
-        def inner(source: str | tuple[str], package: tuple[str]):
-            config_list = (c[name] for c in get_source_configs(source))
-            package_str = ' '.join(package)
-            for config in config_list:
-                if package_required is None:
-                    cmd = config
-                elif package_required:
-                    cmd = config.format(package_str)
-                elif package:
-                    cmd = config['package'].format(package_str)
-                else:
-                    cmd = config['all']
-                os.system(cmd)
-            return func()
-        return inner
-    return decorator
+def subcommand(action: str, source_multiple: bool = True, package_required: bool | None = True):
+    @cli.command(action)
+    @support_source(multiple=source_multiple)
+    @support_package(required=package_required)
+    def _(source: str | tuple[str], package: tuple[str]):
+        if isinstance(source, str):
+            source = (source,)
+        config_list = get_source_configs(source, action)
+        package_str = ' '.join(package)
+        source_str = ', '.join(source)
+        click.secho(f':: {action} packages {package_str} with {source_str}', bold=True)
+        for config in config_list:
+            if package_required is None:
+                cmd = config
+            elif package_required:
+                cmd = config.format(package_str)
+            elif package:
+                cmd = config['package'].format(package_str)
+            else:
+                cmd = config['all']
+            os.system(cmd)
 
 
 @click.group()
 def cli(): pass
 
-@subcommand(source_multiple=False)
-def install(): pass
 
-@subcommand(package_required=False)
-def list(): pass
-
-@subcommand()
-def search(): pass
-
-@subcommand(package_required=False)
-def update(): pass
-
-@subcommand(source_multiple=False)
-def remove(): pass
-
-@subcommand(package_required=None)
-def clean(): pass
+subcommand('install', source_multiple=False)
+subcommand('list', package_required=False)
+subcommand('search')
+subcommand('update', package_required=False)
+subcommand('remove', source_multiple=False)
+subcommand('clean', package_required=None)
 
 
 if __name__ == '__main__':
