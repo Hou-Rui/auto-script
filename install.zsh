623f4dc924c4aa3b75fb5ff16e2a5893af1942fa
Hou Rui <hourui@realtek-sg.com>
2025-08-18 11:36:54 +0800

    refine install.zsh error handling

--- install.zsh
+++ install.zsh
@@ -1,12 +1,17 @@
 #!/usr/bin/env zsh
 
+set -eu
+
 PREFIX="${PREFIX:-/usr/local}"
 ZSH_CUSTOM="${ZSH_CUSTOM:-$PREFIX/share/zshcustom}"
 
 local script="$PREFIX/bin/auto"
 local compdir="$ZSH_CUSTOM/zsh-auto-script"
 
-setopt verbose
+echo "Installing $PWD/auto to $script..."
 install -m755 auto "$script"
+echo "Installing $PWD/zsh-auto-script to $compdir..."
 [[ -d "$compdir" ]] && rm -rf "$compdir"
 cp -r zsh-auto-script "$compdir"
+echo "Done."
+
